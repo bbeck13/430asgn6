ccf596bc0afc8f6b6c5ef0ea9d54296f982c19aa
bbeck13 <badmin@localhost.localdomain>
2016-02-27 13:06:29 -0800

    initialize

---
+++ asgn6.R
@@ -0,0 +1,20 @@
+pg1 = list("+", 2, 1)
+#list("+", 2, 1)
+num = 1
+parse <- function(prog) {
+  if (typeof(prog) == "list"){
+    if (typeof(prog[[1]]) == "character") {
+      op <- prog[[1]]
+      if (op == "+")
+        return((parse(prog[[2]]) + parse(prog[[3]])))
+    } else {
+      return("Not yet")
+    }
+  } else if (typeof(prog) == "double") {
+    return (prog)
+  }
+  else
+    return("error input must be list")
+}
+parse(pg1)
+parse(num)
