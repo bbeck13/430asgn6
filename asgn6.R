b750c11e889b59e5ff26be24a388d3247a28f16a
bbeck13 <badmin@localhost.localdomain>
2016-02-27 13:32:18 -0800

    a

--- asgn6.R
+++ asgn6.R
@@ -1,20 +1,23 @@
 pg1 = list("+", list("+", 2, 1), 1)
+badpg = list("+", list("+", 2, 1), 1)
 num = 1
-num = simpleError("Expected Number", call = NULL)
+
 evaulate <- function(prog) {
-  if (typeof(prog) == "list"){
+  if (typeof(prog) == "list") {
     if (typeof(prog[[1]]) == "character") {
       op <- prog[[1]]
       if (op == "+")
-        return((parse(prog[[2]]) + parse(prog[[3]])))
+        return((evaulate(prog[[2]]) + evaulate(prog[[3]])))
     } else {
       return("Not yet")
     }
   } else if (typeof(prog) == "double") {
     return (prog)
-  }
-  else
-    return("error input must be list")
+  } else if (typeof(prog) == "logical") {
+    return (prog) 
+  } else
+    simpleError("Input must be list, number, or boolean")
 }
-parse(pg1)
-parse(num)
+
+evaulate(pg1)
+evaulate(num)
