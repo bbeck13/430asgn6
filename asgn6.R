52c5881fbe8acf08beefd72afff7315e8bb234b6
bbeck13 <badmin@localhost.localdomain>
2016-02-27 13:50:46 -0800

    a

--- asgn6.R
+++ asgn6.R
@@ -7,18 +7,22 @@ evaluate <- function(prog) {
   if (typeof(prog) == "list"){
     if (typeof(prog[[1]]) == "character") {
       op <- prog[[1]]
-      if (op == "+")
-        return((evaluate(prog[[2]]) + evaluate(prog[[3]])))
-    } else {
-      return("Not yet")
-    }
+        if (length(pg1) != 3) {
+          signalCondition(simpleError("Binop expects 2 args", call = NULL))
+        } else if (op == "+") {
+          return(evaluate(prog[[2]]) + evaluate(prog[[3]]))
+        } else {
+          signalCondition(simpleError("Bad Binop", call = NULL))
+        }
+      }
   } else if (typeof(prog) == "double") {
     return (prog)
   } else if (typeof(prog) == "logical") {
     return (prog) 
   } else
-    simpleError("Input must be list, number, or boolean")
+    signalCondition(simpleError("Input must be list, number, or boolean", call = NULL))
 }
 
 checkEquals(evaluate(pg1), 4)
 checkEquals(evaluate(num), 1)
+checkException(evaluate("bad"))
